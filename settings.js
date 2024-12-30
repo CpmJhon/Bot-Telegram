bc80bb765b3c8b56fc615e17b15dadce8960c33c
Cpm_jhon <130876841+CpmJhon@users.noreply.github.com>
2024-12-30 23:50:17 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,29 @@
+/*
+     Recode By Alfi
+     Original Script By Rafael
+     Base by Zeeone
+     thanks to wannoffc
+*/
+const fs = require("fs");
+
+global.BOT_TOKEN = "7902101355:AAHROCx5bKgR_H4x2yTSIZkNXw9qVY4iGOg" // create a bot here https://t.me/Botfather and get bot tokens
+global.BOT_NAME = "Cpm_Jhon BOTZ" //your bot name
+global.OWNER_NAME = "CPM_JHON" //your name
+global.OWNER_NUMBER = "6285894955362" //your telegram number
+global.OWNER = ["https://t.me/MIKUMIKI", "https://t.me/NATURALSILK"] // create a bot here https://t.me/Botfather and get bot tokens
+const {
+   indonesia
+} = require("./lib");
+global.language = indonesia
+global.lang = language
+global.adminId = "7902101355"
+global.pp = 'https://files.catbox.moe/tttkp9.jpg',
+    //SERVER 1
+  global.domain = '', // Fill in the domain used
+  global.plta = '', // Fill in the appropriate hydropower value
+  global.pltc = '', // Fill in the appropriate pltc value
+  
+  //CREATE PANEL
+  global.loc = '1', // Fill in the desired location
+  global.eggs = '15'
+  
